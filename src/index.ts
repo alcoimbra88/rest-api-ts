120cfe0634679928e3b515c3ef24d12373dd61ed
alan <al_coimbra@hotmail.com>
2021-10-10 21:13:08 -0300

    First commit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response, NextFunction } from 'express';
+import statusRoute from './routes/status.route';
+
+import usersRoute from './routes/uses.route';
+
+const app = express();
+
+
+// App config
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// Routes config
+app.use(usersRoute);
+app.use(statusRoute);
+
+// Server
+app.listen(3000, () => {
+    console.log('App running at port 3000');
+    console.log('localhost:3000/status');
+});
---
+++ src/routes/uses.route.ts
@@ -0,0 +1,37 @@
+import { Router, Request, Response, NextFunction } from "express";
+import { StatusCodes } from 'http-status-codes'
+
+const usersRoute = Router();
+
+usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
+    const users = [{ userName: 'Alan' }]
+    res.status(StatusCodes.OK).send(users);
+})
+
+usersRoute.get('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+    //dataBase.getUserByUUid(uuid)
+    res.status(StatusCodes.OK).send({ uuid })
+})
+
+usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
+    const newUser = req.body
+    res.status(StatusCodes.CREATED).send(newUser)
+});
+
+usersRoute.put('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const uui = req.params.uuid;
+    const modifiedUser = req.body;
+
+    modifiedUser.uui = uui;
+
+    res.status(StatusCodes.OK).send({ modifiedUser })
+})
+
+usersRoute.delete('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+    //dataBase.getUserByUUid(uuid)
+    res.status(StatusCodes.OK).send(` User id:${uuid} Deleted`)
+})
+
+export default usersRoute;
